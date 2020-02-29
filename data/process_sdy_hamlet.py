48e5420061eb7b75c9ad634860d4563350008b2a
Zheng Lian <z.lian@goldwind.com.cn>
2020-02-29 12:11:55 -0800

    final sdy

--- data/process_sdy_hamlet.py
+++ data/process_sdy_hamlet.py
@@ -1,6 +1,9 @@
 READ_PATH = "/Users/zhengl/Downloads/translator-attribution/data/SDY-Hamlet-NoAnnotations.txt"
 WRITE_PATH = "/Users/zhengl/Downloads/translator-attribution/data/SDY-Hamlet-Preprocess-Single-Lines-With_Names.txt"
 
+READ_PATH2 = "/Users/zhengl/Downloads/translator-attribution/data/SDY-Hamlet-Preprocessed-No_Names.txt"
+WRITE_PATH2 = "/Users/zhengl/Downloads/translator-attribution/data/SDY-Hamlet-Preprocessed-Final.txt"
+
 '''
 Types of lines:
 1. Begins with '[', but doesn't end with ']'. Characters move on/off stage.
@@ -16,27 +19,30 @@ Types of lines:
 *** Currently, the names are still inside, wondering if we should
 '''
 def process():
-    with open(READ_PATH) as read_f:
-        with open(WRITE_PATH, 'w') as write_f:
+    # with open(READ_PATH) as read_f:
+    #     with open(WRITE_PATH, 'w') as write_f:
+    #         content = read_f.readlines()
+    #         str = ""
+    #         for line in content:
+    #             # Type 3
+    #             if line == "\n":
+    #                 continue
+    #             # Type 1, 2, 6
+    #             elif line[0] in "第[" and line[1] != "伶":
+    #                 write_f.write(line)
+    #             else:
+    #                 # Type 5
+    #                 if line[0] == "　":
+    #                     str += line.strip()
+    #                 else:
+    #                     if str:
+    #                         write_f.write(str + '\n')
+    #                     str = line.strip()
+    with open(READ_PATH2) as read_f:
+        with open(WRITE_PATH2, 'w') as write_f:
             content = read_f.readlines()
-            str = ""
             for line in content:
-                # Type 3
-                if line == "\n":
-                    continue
-                # Type 1, 2, 6
-                elif line[0] in "第[" and line[1] != "伶":
-                    write_f.write(line)
-                else:
-                    # Type 5
-                    if line[0] == "　":
-                        str += line.strip()
-                    else:
-                        if str:
-                            write_f.write(str + '\n')
-                        str = line.strip()
-
-
+                write_f.write(line.strip() + '\n')
 
 if __name__ == '__main__':
     process()
