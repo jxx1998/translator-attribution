9eccbdea5892ce3fe58d71366de859ee652c8fa9
Zheng Lian <z.lian@goldwind.com.cn>
2020-03-11 01:45:44 -0700

    some progress with BZL Macbeth

--- data/BZL_Macbeth_Preprocess.py
+++ data/BZL_Macbeth_Preprocess.py
@@ -11,11 +11,15 @@ for line in lines:
     if line:
         processed1.append(line)
 
-characters = ["女巫甲 ",
-                        ]
+characters = "女巫甲 |女巫乙 |女巫丙 |顿 |玛 |队长 |列 |麦 |麦夫人 |多 |罗 |门 |安 |凯 |弗 |西 |士兵 |小西 |瑟 |医 |三女巫 "
+processed2 = []
 for line in processed1:
+    line = line.strip()
+    parsed = [token.strip() + '\n' for token in re.split(characters, line) if token]
+    print(parsed)
+    processed2.extend(parsed)
 
 
-out_file = open("BZL-Macbeth-Preprocessed2.txt", "w+")
-out_file.writelines(processed)
+out_file = open("BZL-Macbeth-Preprocessed3.txt", "w+")
+out_file.writelines(processed2)
 out_file.close()
